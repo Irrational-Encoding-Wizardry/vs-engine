b19737c116cdb3b876a64a78be6f18be955fac2c
Sarah <cid+git@cid-chan.moe>
2022-03-27 22:29:39 +0200

    project: Test py39 and a recent vs commit.

--- flake.nix
+++ flake.nix
@@ -4,54 +4,123 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  # VS latest
+  inputs.vs_latest_vs = {
+    url = "github:vapoursynth/vapoursynth";
+    flake = false;
+  };
+  inputs.vs_latest_zimg = {
+    url = "github:sekrit-twc/zimg/v3.0";
+    flake = false;
+  };
+
+  # VS R57
+  inputs.vs_57_vs = {
+    url = "github:vapoursynth/vapoursynth/R57";
+    flake = false;
+  };
+  inputs.vs_57_zimg = {
+    url = "github:sekrit-twc/zimg/v3.0";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }@releases:
+    let
+      default_python = "310";
+
+      latest_is_version = 58;
+      py_versions = [ "39" "310" ];
+      vs_versions = [ 57 "latest" ];
+
+      module = pkgs: vapoursynth: ps: ps.buildPythonPackage rec {
+        pname = "vsengine";
+        version = "r" + (pkgs.lib.strings.sanitizeDerivationName (builtins.readFile ./VERSION));
+        pversion = pkgs.lib.removePrefix "r" version;
+        format = "pyproject";
+        src = ./.;
+        buildInputs = [ ps.poetry ps.setuptools vapoursynth ps.trio ps.pyqt5 ];
+      };
+    in
     {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+
+    } // (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
         };
 
-        makeVapourSynthPackage = ps: 
-          # VapourSynth is not added as a proper
-          # python package in nixpkgs. This will change that.
-          (ps.toPythonModule (pkgs.vapoursynth.override {
-            python3 = ps.python;
-          }));
+        findForRelease = release:
+          let
+            prefix = "vs_${toString release}_";
+            filtered = pkgs.lib.filterAttrs (k: v: pkgs.lib.hasPrefix prefix k) releases;
+          in
+          pkgs.lib.mapAttrs' (k: v: { name = pkgs.lib.removePrefix prefix k; value = v; }) filtered;
 
-        lib = python: pkgs.poetry2nix.mkPoetryEnv {
+        makeVapourSynthPackage = release: ps: 
+          let
+            sources = findForRelease release;
+
+            zimg = pkgs.zimg.overrideAttrs (old: {
+              src = sources.zimg;
+            });
+
+            vapoursynth = (pkgs.vapoursynth.overrideAttrs (old: {
+              # Do not override default python.
+              # We are rebuilding the python module regardless, so there
+              # is no need to recompile the vapoursnyht module.
+              src = sources.vs;
+              version = "r" + toString (if release == "latest" then latest_is_version else release) + "";
+              configureFlags = "--disable-python-module" + (if old ? configureFlags then old.configureFlags else "");
+            })).override { zimg = zimg; };
+          in
+          ps.buildPythonPackage {
+            pname = "vapoursynth";
+            inherit (vapoursynth) src version;
+            pversion = pkgs.lib.removePrefix "r" vapoursynth.version;
+            buildInputs = [ ps.cython vapoursynth ];
+            checkPhase = "true";
+          };
+
+        env = release: python: pkgs.poetry2nix.mkPoetryEnv {
           inherit python;
 
           projectDir = ./.;
           overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-            vapoursynth = makeVapourSynthPackage self;
+            vapoursynth = makeVapourSynthPackage release self;
+            pyqt5 = python.pkgs.pyqt5;
           });
           extraPackages = (ps: [
             ps.ipython
           ]);
         };
+
+        makeTest = vapoursynth: python: pkgs.runCommand "test_py${toString python.version}_vs${toString vapoursynth}_run" {} (
+          let 
+            moduleVs = makeVapourSynthPackage vapoursynth;
+            py = python.withPackages (ps: with ps; [
+              (module pkgs (moduleVs ps) ps)
+              (moduleVs ps)
+              trio
+            ]);
+          in 
+          ''
+            touch $out
+            ${py}/bin/python -m unittest discover -s ${./tests}
+          ''
+        );
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            python310.pkgs.poetry
+            pkgs."python${default_python}".pkgs.poetry
             poetry2nix
-            (lib python310)
+            (env 57 pkgs."python${default_python}")
           ];
         };
 
-        apps = {
-          tests310 = {
-            type = "app";
-            program = "${lib pkgs.python310}/bin/python";
-          };
-        };
+        checks = builtins.listToAttrs (map (v: {
+          name = "py${v.py_versions}_vs${toString v.vs_versions}";
+          value = makeTest v.vs_versions (pkgs."python${v.py_versions}");
+        }) (pkgs.lib.cartesianProductOfSets { inherit py_versions vs_versions; }));
       }));
 }
