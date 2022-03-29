efb1e07d40512971f87936254364a0aa5a0d96c4
Sarah <cid+git@cid-chan.moe>
2022-03-29 23:38:38 +0200

    .f: Add darwin to the lock.

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
     in
     {
 
-    } // (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    } // (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
