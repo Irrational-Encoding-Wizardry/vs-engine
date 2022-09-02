fdb2d21ec6da4f6b189bc85227dc7c17553420a4
Sarah Netzsch <cid+git@cid-chan.moe>
2022-09-02 14:39:59 +0200

    unittests: Make the unittests be discoverable on windows.

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -4,21 +4,26 @@ import platform
 import unittest
 import subprocess
 
+import vsengine.unittest
+
 
 DIR = os.path.dirname(__file__)
 PATH = os.path.join(DIR, "fixtures")
 
 
 def run_fixture(fixture: str, expect_status: int = 0):
-    if platform.system() != "Windows":
-        path = os.path.join(PATH)
-        if "PYTHONPATH" in os.environ:
-            path += os.pathsep + os.environ["PYTHONPATH"]
-        else:
-            path += os.pathsep + os.path.abspath(os.path.join(".."))
-        env = {"PYTHONPATH" : path}
+    path = os.path.join(PATH)
+    if "PYTHONPATH" in os.environ:
+        path += os.pathsep + os.environ["PYTHONPATH"]
     else:
-        env = None
+        path += os.pathsep + os.path.abspath(os.path.join(".."))
+
+    # Find addtional path directories.
+    if platform.system() == "Windows":
+        path += os.pathsep + os.path.dirname(platform.__file__)
+        path += os.pathsep + os.path.dirname(os.path.dirname(vsengine.unittest.__file__))
+
+    env = {"PYTHONPATH" : path}
 
     process = subprocess.run(
         [sys.executable, "-m", "vsengine.unittest", fixture],
@@ -29,6 +34,7 @@ def run_fixture(fixture: str, expect_status: int = 0):
     if process.returncode != expect_status:
         print(process.stdout.decode(sys.getdefaultencoding()), file=sys.stderr)
         assert False
+
 
 class TestUnittestWrapper(unittest.TestCase):
     
